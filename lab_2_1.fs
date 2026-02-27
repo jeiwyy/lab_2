c71696c8adc0a31c5458732e43d9ec3d0653258e
jeiwyy <jeiwy1333@gmail.com>
2026-02-27 12:11:29 +0500

    added lab_2_1

---
+++ lab_2_1.fs
@@ -0,0 +1,22 @@
+// Получить список из длин строк, содержащихся в исходном списке
+open System
+
+let rec createList inpList = 
+    printf "Введите добавляемое значение(0 - конец): "
+    let addVal = Console.ReadLine()
+    match addVal with
+    | "0" -> inpList
+    | _ -> createList (inpList @ [addVal])
+
+
+
+
+[<EntryPoint>]
+let main args = 
+    printfn "Создание списка"
+    let ourList  = createList []
+    printfn "Созданный список: %A" ourList
+    let resList = List.map String.length ourList 
+    printfn "Список длин строк исходного списка: "
+    printfn "%A" resList
+    0
